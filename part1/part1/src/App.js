8be6de20234f2e5d0fab643c45853a8f1ce62bd5
eaybgui <eelliieess@gmail.com>
2023-01-31 17:59:54 +0100

    Creating the possibility to add and list notes

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,18 +1,50 @@
-import './App.css';
+import {Note} from './Note'
+import {useState} from 'react'
 
-const Mensaje = (props) => {
-    return <h1 style={{color: props.color}}>
-            {props.msg}
-        </h1>
-}
+export default function App(props) {
+    
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState("")
+    const [showAll, setShowAll] = useState(true)
 
-function App() {
+    const handleChange = (event) =>{
+        setNewNote(event.target.value)
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault() // para evitar que se refresque la pagina
+        console.log(newNote)
+        const noteToAddToState = {
+            id: notes.length + 1,
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() < 0.5
+        }
+        //cada vez que queremos actualizar el estado queremos crear un array nuevo
+        setNotes(notes.concat(noteToAddToState))
+        setNewNote("") //para limpiar el input
+    }
+    const handleShowAll = () =>{
+        setShowAll(() => !showAll)
+    }
 
     return ( 
-        <div className = "App" > 
-            <Mensaje color='green' msg = 'Pero bueno mi compadre'/>
+        <div>
+            <button onClick={handleShowAll}>{showAll ? "Show only important" : "Show all"}</button>
+            <ol>
+                {notes
+                .filter(note => {
+                    if(showAll === true) return true
+                    return note.important === true
+                })
+                .map((note) =>
+                 <Note key={note.id} {...note}/>
+                 )}
+            </ol>
+            <form onSubmit={handleSubmit}>
+                <input type='text' onChange={handleChange} value={newNote}></input>
+                <button>Crear nota</button>
+            </form>
         </div>
-    );
+    )
 }
-
-export default App;
