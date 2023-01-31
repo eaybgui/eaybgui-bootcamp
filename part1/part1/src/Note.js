b2d5cfbb3ad03a57f3ee7d5ec8978754b9d07919
eaybgui <eelliieess@gmail.com>
2023-01-31 18:40:23 +0100

    Adding functionalities: list notes, add notes

---
+++ part1/part1/src/Note.js
@@ -0,0 +1,12 @@
+export const Note = (props) => {
+    const {content, date} = props
+
+    return (
+        <li >
+            <p>{content}</p>
+            <small><time>{date}</time>
+            </small>
+        </li>
+    )
+}
+
